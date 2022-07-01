cad1cad006926aeb7f17eb7b057fb54647f520af
AdrianOrcik <adrian.orcik@gmail.com>
2022-07-01 14:00:43 +0300

    Initial package files

---
+++ Runtime/CollisionExtensions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSdk.Core
+{
+    public class CollisionExtensions : MonoBehaviour
+    {
+
+    }
+}
---
+++ Runtime/GameUtilities.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSdk.Core
+{
+    public class GameUtilities : MonoBehaviour
+    {
+        public static string Version = "1.0.0";
+        public static string SomethingElse = "Something Else";
+    }
+}
