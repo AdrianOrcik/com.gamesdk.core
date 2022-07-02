dbc34dc58349fddbf76ac7127574c8d08eb314c8
AdrianOrcik <adrian.orcik@gmail.com>
2022-07-02 23:39:03 +0300

    Extend singleton class

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Singleton : MonoBehaviour
 {
     public static string Instance = "SingletonInstance";
+    public static string AnotherInstance = "AnotherSingletonInstance";
 }
